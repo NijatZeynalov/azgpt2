2c79ef86c0e1148e6f32fde6d45a0df61ee86206
Nijat Zeynalov <31247506+NijatZeynalov@users.noreply.github.com>
2023-03-01 23:57:05 +0400

    Create news_to_txt.py

---
+++ news/lentaz/news_to_txt.py
@@ -0,0 +1,19 @@
+df = pd.read_csv('lent_az_urls.csv')
+
+def news_to_txt(url_list):
+    
+    for base_url in url_list:
+    
+        file_name = base_url.split('/')[-1]
+
+        reqs = requests.get(base_url)
+        soup = BeautifulSoup(reqs.text, 'html.parser')
+        mydivs = soup.find("div", {"class": "news_content"})
+
+        with open(f'{file_name}.txt', 'w') as f:
+            f.write(mydivs.text)
+            
+            
+n = 50000
+for g, chunck_df in df.groupby(np.arange(len(df)) // n):
+    news_to_txt(chunck_df['0'])
